4c3efc1b32a0e83379be886ff9454f1f5c74fef4
mohamed-Fathy1 <mohamedfathysaied18@gmail.com>
2023-07-14 04:54:32 +0300

    Update Base_module class

--- models/base_model.py
+++ models/base_model.py
@@ -1,37 +1,65 @@
 #!/usr/bin/python3
-import uuid
-from datetime import datetime
 '''
 Base class module defines all common attributes/methods for other classes
 '''
+import uuid
+from datetime import datetime
 
 
 class BaseModel():
-    '''
-    Base class common attributes/methods for other classes
-    '''
+    """Base class for other classes.
+
+    Attributes:
+        id (str): The unique identifier of the object.
+        created_at (datetime): The date and time the object was created.
+        updated_at (datetime): The date and time the object was last updated.
+    """
+
     def __init__(self, *args, **kwargs):
+        """Instantiate a BaseModel object.
+
+        Args:
+            *args: Variable length argument list.
+            **kwargs: Arbitrary keyword arguments.
+
+        Returns:
+            None.
         """
-        Instantiate BaseModel object
-        """
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.today()
-        self.updated_at = self.created_at
+        if kwargs:
+            if kwargs["__class__"] == self.__class__.__name__:
+                self.id, self.created_at, self.updated_at, *_ = kwargs.values()
+                self.created_at = datetime.strptime(
+                        self.created_at, "%Y-%m-%dT%H:%M:%S.%f")
+                self.updated_at = datetime.strptime(
+                        self.updated_at, "%Y-%m-%dT%H:%M:%S.%f")
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.today()
+            self.updated_at = self.created_at
 
     def __str__(self):
-        """
-        return a string representation of the BaseModel object
+        """Return a string representation of the BaseModel object.
+
+        Returns:
+            str: A string representation of the BaseModel object.
         """
         return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
 
     def save(self):
-        """
-        Update the value of updated_at to the current datetime
+        """Update the value of updated_at to the current datetime.
+
+        Returns:
+            None.
         """
         self.updated_at = datetime.today()
 
     def to_dict(self):
-        my_dict = {**(self.__dict__), "__class__": self.__class__.__name__}
-        my_dict["created_at"] = my_dict["created_at"].isoformat()
-        my_dict["updated_at"] = my_dict["updated_at"].isoformat()
-        return my_dict
+        """Return a dictionary representation of the BaseModel object.
+
+        Returns:
+            dict: A dictionary representation of the BaseModel object.
+        """
+        obj_dict = {**(self.__dict__), "__class__": self.__class__.__name__}
+        obj_dict["created_at"] = obj_dict["created_at"].isoformat()
+        obj_dict["updated_at"] = obj_dict["updated_at"].isoformat()
+        return obj_dict
